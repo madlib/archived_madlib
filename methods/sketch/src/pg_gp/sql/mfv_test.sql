655b0e122078e58fa92bd7fee7267e8884aaf4c6
Joe Hellerstein <jmh@berkeley.edu>
2011-01-24 17:23:56 -0500

    major mfv debugging

--- methods/sketch/src/pg_gp/sql/mfv_test.sql
+++ methods/sketch/src/pg_gp/sql/mfv_test.sql
@@ -2,6 +2,11 @@ set search_path to "$user",public,madlib;
 
 -- Basic methods
 select mfvsketch_top_histogram(i,5) 
-  from (select * from generate_series(1,100) union all select * from generate_series(10,15)) as T(i);
+from (select * from generate_series(1,100) union all select * from generate_series(10,15)) as T(i);
 select mfvsketch_top_histogram(utc_offset,5) from pg_timezone_names;
 select mfvsketch_top_histogram(NULL::bytea,5) from generate_series(1,100);
+
+select mfvsketch_quick_histogram(i,5) 
+from (select * from generate_series(1,100) union all select * from generate_series(10,15)) as T(i);
+select mfvsketch_quick_histogram(utc_offset,5) from pg_timezone_names;
+select mfvsketch_quick_histogram(NULL::bytea,5) from generate_series(1,100);
