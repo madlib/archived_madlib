b47ddc8dfa2ab815143620438cfc6159d23cb7d2
Kee Siong Ng <keesiong.ng@emc.com>
2011-03-30 16:03:49 +1100

    Reimplemented novelty detection routine in C. Corrected margin update formula in classification routine. General code clean up.

--- methods/kernel-machines/src/pg_gp/online_sv.c
+++ methods/kernel-machines/src/pg_gp/online_sv.c
@@ -15,6 +15,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <float.h>
 
 #ifdef PG_MODULE_MAGIC
 PG_MODULE_MAGIC;
@@ -41,8 +42,9 @@ static float8 svm_predict_eval(float8 * weights, float8 * support_vectors,
 			       float8 * ind, int32 nsvs, int32 ind_dim)
 {
 	int i; float8 ret = 0;
-	for (i=0; i!=nsvs; i++)
+	for (i=0; i!=nsvs; i++) {
 		ret += weights[i] * kernel_dot(support_vectors,i,ind_dim,ind);
+	}
 	return ret;
 }
 
@@ -154,24 +156,19 @@ static ArrayType * addNewSV(ArrayType * spvs, float8 * ind, int nsvs, int dim)
 	}
 }
 
+Datum svm_reg_update(PG_FUNCTION_ARGS);
+PG_FUNCTION_INFO_V1(svm_reg_update);
+
 /**
- * This is the main online support vector regression learning algorithm. 
+ * This is the online support vector regression learning algorithm. 
+ * The algorithm is set in the nu-SV regression setting.
  * The function updates the support vector model as it processes each new 
  * training example.
  * This function is wrapped in an aggregate function to process all the 
  * training examples stored in a table.  
- * The learning parameters (eta, slambda, and nu) are hardcoded at the moment. 
- * We may want to make them input parameters at some stage, although the naive 
- * user would probably be daunted with the prospect of having to specify them.
  */
-Datum svm_reg_update(PG_FUNCTION_ARGS);
-PG_FUNCTION_INFO_V1(svm_reg_update);
-
 Datum svm_reg_update(PG_FUNCTION_ARGS)
 {
-	float8 eta = 0.05;    // learning rate
-	float8 slambda = 0.2; // regularisation parameter
-	float8 nu = 0.001;    // compression parameter in [0,1]
 	float8 p;             // prediction for data point
 	float8 diff;          // difference between p and target label
 	float8 error;         // absolute value of diff
@@ -183,12 +180,21 @@ Datum svm_reg_update(PG_FUNCTION_ARGS)
 	HeapTupleHeader t = PG_GETARG_HEAPTUPLEHEADER(0);
 	ArrayType * ind_arr = PG_GETARG_ARRAYTYPE_P(1);
 	float8 label = PG_GETARG_FLOAT8(2);
+	float8 eta = PG_GETARG_FLOAT8(3);
+	float8 nu = PG_GETARG_FLOAT8(4);
+	float8 slambda = PG_GETARG_FLOAT8(5);
+
+	if (eta <= 0 || eta > 1 || nu <= 0 || nu > 1 || eta * slambda > 1)
+		ereport(ERROR,
+			(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+			 errmsg("function \"%s\" called with invalid parameter",
+				format_procedure(fcinfo->flinfo->fn_oid))));
 
 	if (ARR_NULLBITMAP(ind_arr) || ARR_NDIM(ind_arr) != 1 ||
 	    ARR_ELEMTYPE(ind_arr) != FLOAT8OID)
 		ereport(ERROR,
 		       (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-			errmsg("function \"%s\" called with invalid ind parameter",
+			errmsg("function \"%s\" called with invalid parameter",
 			       format_procedure(fcinfo->flinfo->fn_oid))));
 
 	float8 * ind = (float8 *)ARR_DATA_PTR(ind_arr);
@@ -245,11 +251,20 @@ Datum svm_reg_update(PG_FUNCTION_ARGS)
 	inds++;
 	cum_err = cum_err + error;
 
+	float8 cap = 0.1 + 1 / (1 - eta * slambda);
+
 	if (error > epsilon) {
 		// unlike the original algorithm in Kivinen et at, this 
 		// rescaling is only done when we make a large enough error
-		for (i=0; i!=nsvs; i++) 
+		for (i=0; i!=nsvs; i++) {
+			// we need to avoid underflows; cap is designed to 
+			// make sure we never go below DBL_MIN
+			if (weights[i] < (cap + 0.1) * DBL_MIN) { 
+				weights[i] = 0;
+				continue;
+			}
 			weights[i] = weights[i] * (1 - eta * slambda);
+		}
 
 		weight = diff < 0 ? -eta : eta;
 		weights_arr = addNewWeight(weights_arr,weight,nsvs);
@@ -293,21 +308,17 @@ Datum svm_reg_update(PG_FUNCTION_ARGS)
 	PG_RETURN_DATUM(HeapTupleGetDatum(ret));
 }
 
+Datum svm_cls_update(PG_FUNCTION_ARGS);
+PG_FUNCTION_INFO_V1(svm_cls_update);
 
 /**
- * This is the main online support vector classification algorithm. 
+ * This is the online support vector classification algorithm. 
+ * The algorithm is set in the nu-SV classification setting. 
  * The function updates the support vector model as it processes each new 
  * training example.
  * This function is wrapped in an aggregate function to process all the 
  * training examples stored in a table.  
- * The learning parameters (eta and nu) are hardcoded at the moment. 
- * We may want to make them input parameters at some stage, although the 
- * naive user would probably be daunted with the prospect of having to 
- * specify them. 
  */
-Datum svm_cls_update(PG_FUNCTION_ARGS);
-PG_FUNCTION_INFO_V1(svm_cls_update);
-
 Datum svm_cls_update(PG_FUNCTION_ARGS)
 {
 	float8 p;             // label * prediction for data point 
@@ -318,14 +329,22 @@ Datum svm_cls_update(PG_FUNCTION_ARGS)
 	ArrayType * ind_arr = PG_GETARG_ARRAYTYPE_P(1);
 	float8 label = PG_GETARG_FLOAT8(2);
 	float8 eta = PG_GETARG_FLOAT8(3);     // learning rate
-	float8 slambda = PG_GETARG_FLOAT8(4); // regularisation parameter
-	float8 rho = PG_GETARG_FLOAT8(5);     // margin
+	float8 nu = PG_GETARG_FLOAT8(4);  /* compression parameter; about nu
+					   * fraction of the training data will
+					   * become support vectors
+					   */
+
+	if (eta <= 0 || eta > 1 || nu <= 0 || nu > 1)
+		ereport(ERROR,
+			(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+			 errmsg("function \"%s\" called with invalid parameter",
+				format_procedure(fcinfo->flinfo->fn_oid))));
 
 	if (ARR_NULLBITMAP(ind_arr) || ARR_NDIM(ind_arr) != 1 ||
 	    ARR_ELEMTYPE(ind_arr) != FLOAT8OID)
 		ereport(ERROR,
 		       (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
-			errmsg("function \"%s\" called with invalid ind parameter",
+			errmsg("function \"%s\" called with invalid parameter",
 			       format_procedure(fcinfo->flinfo->fn_oid))));
 
 	float8 * ind = (float8 *)ARR_DATA_PTR(ind_arr);
@@ -336,7 +355,7 @@ Datum svm_cls_update(PG_FUNCTION_ARGS)
 	int32 inds = DatumGetInt32(GetAttributeByName(t, "inds", &nil[0]));
 	float8 cum_err =DatumGetFloat8(GetAttributeByName(t,"cum_err",&nil[1]));
 	float8 epsilon =DatumGetFloat8(GetAttributeByName(t,"epsilon",&nil[2]));
-	float8 rho2 = DatumGetFloat8(GetAttributeByName(t, "rho", &nil[3]));
+	float8 rho = DatumGetFloat8(GetAttributeByName(t, "rho", &nil[3]));
 	float8 b = DatumGetFloat8(GetAttributeByName(t, "b", &nil[4]));
 	int32 nsvs = DatumGetInt32(GetAttributeByName(t, "nsvs", &nil[5]));
 	int32 ind_dim =DatumGetInt32(GetAttributeByName(t, "ind_dim", &nil[6]));
@@ -374,11 +393,7 @@ Datum svm_cls_update(PG_FUNCTION_ARGS)
 	float8 * weights = (float8 *)ARR_DATA_PTR(weights_arr);
 	float8 * support_vectors = (float8 *)ARR_DATA_PTR(support_vectors_arr);
 
-	// This is the main classification update algorithm.
-	// When rho = 0 and slambda = 0, this is equiv. to kernel perceptron.
-	// When rho = 0 and slambda > 0, this is equiv. to kernel perceptron 
-	// with regularisation
-	// The standard SVM case is when rho > 0 and slambda > 0.
+	// This is the nu-SV classification update algorithm.
 	p = svm_predict_eval(weights, support_vectors, ind, nsvs, ind_dim) + b; 
 	p = label * p;
 
@@ -388,13 +403,26 @@ Datum svm_cls_update(PG_FUNCTION_ARGS)
 	if (p <= rho) {
 		// unlike the original algorithm in Kivinen et at, this 
 		// rescaling is only done when we make a large enough error
-		for (i=0; i!=nsvs; i++) 
-			weights[i] = weights[i] * (1 - eta * slambda);
+		for (i=0; i!=nsvs; i++) {
+			// we need to avoid underflows; the weight discounting
+			// never multiply a weight by less than 0.9, 
+			// and 1.15 * 0.9 > 1
+			if (weights[i] < 1.15 * DBL_MIN) { 
+				weights[i] = 0;
+				continue;
+			}
+			// we set lambda = 0.1 here, the exact value of lambda
+			// is mathematically irrelevant
+			weights[i] = weights[i] * (1 - 0.1*eta); 
+		}
 
 		weights_arr = addNewWeight(weights_arr,label * eta,nsvs);
 	        support_vectors_arr = addNewSV(support_vectors_arr,ind,nsvs,ind_dim);
 		nsvs++;
 		b = b + eta * label;
+		rho = rho - eta * (1 - nu);
+	} else {
+		rho = rho + eta * nu; 
 	}
 
 	// Package up the attributes and return the resultant composite object
@@ -430,3 +458,147 @@ Datum svm_cls_update(PG_FUNCTION_ARGS)
 	PG_RETURN_DATUM(HeapTupleGetDatum(ret));
 }
 
+Datum svm_nd_update(PG_FUNCTION_ARGS);
+PG_FUNCTION_INFO_V1(svm_nd_update);
+
+/**
+ * This is the online support vector novelty detection algorithm. 
+ * The algorithm is set in the nu-SV setting. 
+ * The function updates the support vector model as it processes each new 
+ * training example.
+ * This function is wrapped in an aggregate function to process all the 
+ * training examples stored in a table.  
+ */
+Datum svm_nd_update(PG_FUNCTION_ARGS)
+{
+	float8 p;             // prediction for data point 
+	int i;
+
+	// Get the input arguments and check for errors
+	HeapTupleHeader t = PG_GETARG_HEAPTUPLEHEADER(0);
+	ArrayType * ind_arr = PG_GETARG_ARRAYTYPE_P(1);
+	float8 eta = PG_GETARG_FLOAT8(2);     // learning rate
+	float8 nu = PG_GETARG_FLOAT8(3);  /* compression parameter; about nu
+					   * fraction of the training data will
+					   * become support vectors
+					   */
+
+	if (eta <= 0 || eta > 1 || nu <= 0 || nu > 1)
+		ereport(ERROR,
+			(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+			 errmsg("function \"%s\" called with invalid parameter",
+				format_procedure(fcinfo->flinfo->fn_oid))));
+
+	if (ARR_NULLBITMAP(ind_arr) || ARR_NDIM(ind_arr) != 1 ||
+	    ARR_ELEMTYPE(ind_arr) != FLOAT8OID)
+		ereport(ERROR,
+		       (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+			errmsg("function \"%s\" called with invalid parameter",
+			       format_procedure(fcinfo->flinfo->fn_oid))));
+
+	float8 * ind = (float8 *)ARR_DATA_PTR(ind_arr);
+
+	// Read the attributes of the input support vector model
+	bool nil[9] = { 0,0,0,0,0,0,0,0,0 };
+
+	int32 inds = DatumGetInt32(GetAttributeByName(t, "inds", &nil[0]));
+	float8 cum_err =DatumGetFloat8(GetAttributeByName(t,"cum_err",&nil[1]));
+	float8 epsilon =DatumGetFloat8(GetAttributeByName(t,"epsilon",&nil[2]));
+	float8 rho = DatumGetFloat8(GetAttributeByName(t, "rho", &nil[3]));
+	float8 b = DatumGetFloat8(GetAttributeByName(t, "b", &nil[4]));
+	int32 nsvs = DatumGetInt32(GetAttributeByName(t, "nsvs", &nil[5]));
+	int32 ind_dim =DatumGetInt32(GetAttributeByName(t, "ind_dim", &nil[6]));
+	ArrayType * weights_arr = 
+		DatumGetArrayTypeP(GetAttributeByName(t, "weights", &nil[7]));
+	ArrayType * support_vectors_arr = 
+		DatumGetArrayTypeP(GetAttributeByName(t,"individuals",&nil[8]));
+
+	for (i=0; i!=9; i++)
+		if (nil[i]) elog(ERROR, "error reading support vector model");
+
+	// The first time this function is called, the initial state doesn't
+	// tell us the dimension of the data points; this needs to be extracted
+	// from the ind argument. 
+	if (ind_dim == 0) {
+		int ndims = ARR_NDIM(ind_arr);
+		int * dims = ARR_DIMS(ind_arr);
+		ind_dim = ArrayGetNItems(ndims, dims);
+	} 
+	// Also, initially the weights_arr and support_vectors_arr arrays 
+	// are empty, so we can't do a dimension check until there are
+	// support vectors.
+	if (nsvs > 0) {
+		if (ARR_NULLBITMAP(weights_arr) || ARR_NDIM(weights_arr) != 1 ||
+		    ARR_ELEMTYPE(weights_arr) != FLOAT8OID || 
+		    ARR_NULLBITMAP(support_vectors_arr) || 
+		    ARR_NDIM(support_vectors_arr) != 1 ||
+		    ARR_ELEMTYPE(support_vectors_arr) != FLOAT8OID) 
+			ereport(ERROR,
+				(errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+			errmsg("function \"%s\" called with invalid array parameters",
+			       format_procedure(fcinfo->flinfo->fn_oid))));
+	}
+
+	float8 * weights = (float8 *)ARR_DATA_PTR(weights_arr);
+	float8 * support_vectors = (float8 *)ARR_DATA_PTR(support_vectors_arr);
+
+	// This is the nu-SV novelty detection update algorithm.
+	p = svm_predict_eval(weights, support_vectors, ind, nsvs, ind_dim); 
+	inds++;
+
+	if (p < rho) {
+		// unlike the original algorithm in Kivinen et at, this 
+		// rescaling is only done when we make a large enough error
+		for (i=0; i!=nsvs; i++) {
+			// we need to avoid underflows; the weight discounting
+			// never multiply a weight by less than 0.9, 
+			// and 1.15 * 0.9 > 1
+			if (weights[i] < 1.15 * DBL_MIN) { 
+				weights[i] = 0;
+				continue;
+			}
+			// we set lambda = 0.1 here, the exact value of lambda
+			// is mathematically irrelevant
+			weights[i] = weights[i] * (1 - 0.1*eta); 
+		}
+
+		weights_arr = addNewWeight(weights_arr,eta,nsvs);
+	        support_vectors_arr = addNewSV(support_vectors_arr,ind,nsvs,ind_dim);
+		nsvs++;
+		rho = rho - eta * (1 - nu);
+	} else {
+		rho = rho + eta * nu; 
+	}
+
+	// Package up the attributes and return the resultant composite object
+	Datum values[9];
+	values[0] = Int32GetDatum(inds);
+	values[1] = Float8GetDatum(cum_err);
+	values[2] = Float8GetDatum(epsilon);
+	values[3] = Float8GetDatum(rho);
+	values[4] = Float8GetDatum(b);
+	values[5] = Int32GetDatum(nsvs);
+	values[6] = Int32GetDatum(ind_dim);
+	values[7] = PointerGetDatum(weights_arr);
+	values[8] = PointerGetDatum(support_vectors_arr);
+
+	TupleDesc tuple;
+	if (get_call_result_type(fcinfo, NULL, &tuple) != TYPEFUNC_COMPOSITE)
+		ereport(ERROR,
+			(errcode( ERRCODE_FEATURE_NOT_SUPPORTED ),
+			 errmsg( "function returning record called in context "
+				 "that cannot accept type record" )));
+	tuple = BlessTupleDesc(tuple);
+
+	bool * isnulls = palloc0(9 * sizeof(bool));
+	HeapTuple ret = heap_form_tuple(tuple, values, isnulls);
+
+	for (i=0; i!=9; i++)
+		if (isnulls[i])
+			ereport(ERROR,
+		       (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+			errmsg("function \"%s\" produced null results %d",
+			       format_procedure(fcinfo->flinfo->fn_oid),i)));
+
+	PG_RETURN_DATUM(HeapTupleGetDatum(ret));
+}
