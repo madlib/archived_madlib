a700dd49b49bd7742eb1c441a5cc432e0e50811f
Florian Schoppmann <Florian.Schoppmann@emc.com>
2011-06-10 14:19:01 -0700

    Finding PostgreSQL and Greenplum now relies on finding pg_config. Use -DPOSTGRESQL_PG_CONFIG=/path/to/pg_config or -DGREENPLUM_PG_CONFIG=... to specify the the path to pg_config manually

--- cmake/FindGreenplum.cmake
+++ cmake/FindGreenplum.cmake
@@ -6,8 +6,10 @@
 #
 # This module sets the following variables:
 #  GREENPLUM_FOUND - set to true if headers and binary were found
-#  GREENPLUM_BASE_DIR - Greenplum base directory
-#  GREENPLUM_INCLUDE_DIR - list of include directories
+#  GREENPLUM_LIB_DIR - Greenplum base directory
+#  GREENPLUM_CLIENT_INCLUDE_DIR - client include directory
+#  GREENPLUM_SERVER_INCLUDE_DIR - server include directory
+#  GREENPLUM_ADDITIONAL_INCLUDE_DIRS - additional needed include directories
 #  GREENPLUM_EXECUTABLE - path to Greenplum's postgres binary
 #  GREENPLUM_VERSION_MAJOR - major version number
 #  GREENPLUM_VERSION_MINOR - minor version number
@@ -24,52 +26,66 @@
 # checks is the least we require...
 cmake_minimum_required(VERSION 2.6.3)
 
-# Note: HINTS get looked at before PATHS, see find_program documentation
-find_path(GREENPLUM_INCLUDE_DIR
-	NAMES postgresql/server/postgres.h postgresql/server/fmgr.h
-	HINTS 
-	/usr/local/greenplum-db/include
-    "$ENV{GPHOME}/include"
-)
+if(NOT GREENPLUM_PG_CONFIG)
+    find_program(GREENPLUM_PG_CONFIG pg_config
+        HINTS /usr/local/greenplum-db/bin
+            "$ENV{GPHOME}/bin"
+    )
+endif(NOT GREENPLUM_PG_CONFIG)
 
-if(GREENPLUM_INCLUDE_DIR)
-    # Unfortunately, the regex engine of CMake is rather primitive.
-    # It treats the following the same as "^(.*)/[^/]*$"
-    string(REGEX REPLACE "^(.*)/([^/]|\\/)*\$" "\\1" GREENPLUM_BASE_DIR ${GREENPLUM_INCLUDE_DIR})
-    set(GREENPLUM_EXECUTABLE ${GREENPLUM_BASE_DIR}/bin/postgres)
-    
-    set(GREENPLUM_INTERNAL_INCLUDE_DIR ${GREENPLUM_INCLUDE_DIR}/postgresql/internal)
-    set(GREENPLUM_INCLUDE_DIR ${GREENPLUM_INCLUDE_DIR}/postgresql/server)
-endif(GREENPLUM_INCLUDE_DIR)
+if(GREENPLUM_PG_CONFIG)
+    execute_process(COMMAND ${GREENPLUM_PG_CONFIG} --includedir
+        OUTPUT_VARIABLE GREENPLUM_CLIENT_INCLUDE_DIR
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+endif(GREENPLUM_PG_CONFIG)
 
-if(NOT EXISTS ${GREENPLUM_EXECUTABLE})
-    unset(GREENPLUM_EXECUTABLE)
-endif(NOT EXISTS ${GREENPLUM_EXECUTABLE})
-#find_program(GREENPLUM_EXECUTABLE
-#	NAMES postgres
-#	HINTS
-#	/usr/local/greenplum-db
-#)
-
-if(GREENPLUM_INCLUDE_DIR)
-    set(GREENPLUM_FOUND, "YES")
-
+if(GREENPLUM_PG_CONFIG AND GREENPLUM_CLIENT_INCLUDE_DIR)
 	set(GREENPLUM_VERSION_MAJOR 0)
 	set(GREENPLUM_VERSION_MINOR 0)
 	set(GREENPLUM_VERSION_PATCH 0)
 	
-	if(EXISTS "${GREENPLUM_INCLUDE_DIR}/pg_config.h")
+	if(EXISTS "${GREENPLUM_CLIENT_INCLUDE_DIR}/pg_config.h")
 		# Read and parse Greenplum version header file for version number
-		file(READ "${GREENPLUM_INCLUDE_DIR}/pg_config.h" _GREENPLUM_HEADER_CONTENTS)
+		file(READ "${GREENPLUM_CLIENT_INCLUDE_DIR}/pg_config.h" _GREENPLUM_HEADER_CONTENTS)
+
+        string(REGEX REPLACE ".*#define PACKAGE_NAME \"([^\"]+)\".*" "\\1" GREENPLUM_PACKAGE_NAME "${_GREENPLUM_HEADER_CONTENTS}")
 		string(REGEX REPLACE ".*#define GP_VERSION \"([^\"]+)\".*" "\\1" GREENPLUM_VERSION_STRING "${_GREENPLUM_HEADER_CONTENTS}")
 		string(REGEX REPLACE "([0-9]+).*" "\\1" GREENPLUM_VERSION_MAJOR "${GREENPLUM_VERSION_STRING}")
 		string(REGEX REPLACE "[0-9]+\\.([0-9]+).*" "\\1" GREENPLUM_VERSION_MINOR "${GREENPLUM_VERSION_STRING}")
 		string(REGEX REPLACE "[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" GREENPLUM_VERSION_PATCH "${GREENPLUM_VERSION_STRING}")
-	endif(EXISTS "${GREENPLUM_INCLUDE_DIR}/pg_config.h")
-endif(GREENPLUM_INCLUDE_DIR)
+	endif(EXISTS "${GREENPLUM_CLIENT_INCLUDE_DIR}/pg_config.h")
 
-# server/funcapi.h ultimately includes server/access/xact.h, from which cdb/cdbpathlocus.h is included
-list(APPEND GREENPLUM_INCLUDE_DIR ${GREENPLUM_INTERNAL_INCLUDE_DIR})
+    if(GREENPLUM_PACKAGE_NAME STREQUAL "Greenplum Database")
+        set(GREENPLUM_FOUND, "YES")
+
+        execute_process(COMMAND ${GREENPLUM_PG_CONFIG} --bindir
+            OUTPUT_VARIABLE GREENPLUM_EXECUTABLE
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+        set(GREENPLUM_EXECUTABLE "${GREENPLUM_EXECUTABLE}/postgres")
+
+        execute_process(COMMAND ${GREENPLUM_PG_CONFIG} --includedir-server
+            OUTPUT_VARIABLE GREENPLUM_SERVER_INCLUDE_DIR
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+
+        execute_process(COMMAND ${GREENPLUM_PG_CONFIG} --libdir
+            OUTPUT_VARIABLE GREENPLUM_LIB_DIR
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+        
+        # server/funcapi.h ultimately includes server/access/xact.h, from which
+        # cdb/cdbpathlocus.h is included
+        execute_process(COMMAND ${GREENPLUM_PG_CONFIG} --pkgincludedir
+            OUTPUT_VARIABLE GREENPLUM_ADDITIONAL_INCLUDE_DIRS
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+        )
+    else(GREENPLUM_PACKAGE_NAME STREQUAL "Greenplum Database")
+        message(STATUS "Found pg_config at \"${GREENPLUM_PG_CONFIG}\", but it does not point to a Greenplum installation.")
+        unset(GREENPLUM_CLIENT_INCLUDE_DIR)
+    endif(GREENPLUM_PACKAGE_NAME STREQUAL "Greenplum Database")
+endif(GREENPLUM_PG_CONFIG AND GREENPLUM_CLIENT_INCLUDE_DIR)
 
 # find_package_handle_standard_args has VERSION_VAR argument onl since version 2.8.4
 if(${CMAKE_VERSION} VERSION_LESS "2.8.4")
@@ -79,7 +95,9 @@ endif()
 # Checks 'RECQUIRED', 'QUIET' and versions.
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Greenplum
-	REQUIRED_VARS GREENPLUM_INCLUDE_DIR GREENPLUM_EXECUTABLE
+	REQUIRED_VARS GREENPLUM_CLIENT_INCLUDE_DIR GREENPLUM_SERVER_INCLUDE_DIR
+        GREENPLUM_ADDITIONAL_INCLUDE_DIRS
+        GREENPLUM_EXECUTABLE
 	VERSION_VAR GREENPLUM_VERSION_STRING)
 
 if(${CMAKE_VERSION} VERSION_LESS "2.8.4")
