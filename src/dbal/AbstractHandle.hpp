3ab55e0fdd058bfa06e8c32dfbae88b70e4ce9d6
Florian Schoppmann <Florian.Schoppmann@emc.com>
2011-06-19 23:13:01 +0200

    More C++ DBAL documentation

--- src/dbal/AbstractHandle.hpp
+++ src/dbal/AbstractHandle.hpp
@@ -4,9 +4,28 @@
  *
  *//* ----------------------------------------------------------------------- */
 
+
+/**
+ * @brief Abstract base class for a memory handle
+ *
+ * A handle is an opaque reference to a block of memory. An example might
+ * be an array handle: It points to a contiguous block of memory containing
+ * raw data. The meta data (array size, element type, etc.), however,
+ * might be stored separately, in a port-specific fashion. The
+ * platform-independent code will only work with the interface provided by
+ * this acstract base class.
+ */
 class AbstractHandle {
 public:
     virtual ~AbstractHandle() { }
+    
+    /**
+     * @brief Return a pointer to the raw data this handle points to. 
+     */
     virtual void *ptr() = 0;
+    
+    /**
+     * @brief Return a copy of this memory handle.
+     */
     virtual MemHandleSPtr clone() const = 0;
 };
--- src/dbal/AbstractValue_proto.hpp
+++ src/dbal/AbstractValue_proto.hpp
@@ -4,6 +4,16 @@
  *
  *//* ----------------------------------------------------------------------- */
 
+/**
+ * @brief Abstract base class for in- and output types of module functions
+ *
+ * This class provides the interface that MADlib modules use for accessing input
+ * and returning output values.
+ *
+ * Subclasses of AbstractValue can be recursive structures. In this case, values
+ * are called \em compounds and are made up of several elements. Each of these
+ * elements is again an AbstractValue (and can again be a compound).
+ */
 class AbstractValue {
     friend class AnyValue;
 
@@ -27,9 +37,11 @@ public:
     }
         
     /**
+     * @brief Call AbstractValueConverter::convert() with this as argument
+     *
      * This function performs a callback to the specified ValueConverter.
-     * This allows relying on the vtable of ValueConverter for dispatching
-     * conversion requests.
+     * This allows relying on the vtable of AbstractValueConverter for
+     * dispatching conversion requests.
      */
     virtual void convert(AbstractValueConverter &inConverter) const {
     }
