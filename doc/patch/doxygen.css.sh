b3f01ee9666c6d0397ec25b5748687cfbb7d56c7
Florian Schoppmann <Florian.Schoppmann@emc.com>
2011-07-07 12:46:11 +0200

    Doxygen: CSS beautification.
    In Naive Bayes documentation, removed rempty lines after paragraph titles.

--- doc/patch/doxygen.css.sh
+++ doc/patch/doxygen.css.sh
@@ -14,51 +14,57 @@ cat - >> doxygen.css <<'EOF'
 
 /* -- MADlib patches -------------------------------------------------------- */
 
-/* No automtic line wrapping at white spaces in <pre> or \verbatim
-   environments. */
-
-p,pre {  
-    padding-left: 1.8em;  
-}  
-    
-div.contents li {
-    margin-bottom: 1em;
+/* Indent paragraphs in the main text, but not in framed boxes */
+div.contents > p, div.contents > pre, div.contents > ul, div.contents > div.fragment, dd {  
+    margin-left: 20px;
 }
 
+/* Increase spacing between titled paragraphs in the main text, but not in 
+   framed boxes */
+div.contents > dl {
+    margin-top: 2em;
+}
+
+/* Increase spacing between list items in the main text */
+div.contents li {
+    margin-top: 1em;
+}
+
+/* No automtic line wrapping at white spaces in <pre> or \verbatim
+   environments. */
 pre.fragment {
     word-wrap: normal;
 }
 
+/* No padding for paragraph headers (in its infinite wisdom, doxygen uses <dl>
+   environments for that) */
 dl {
     padding-left: 0;
 }
 
+/* Increase the font size for paragraph headers */
 dt {
     font-size: 120%;
     margin-bottom: 1em;
-    margin-top: 2em;
 }
 
-dd {
-    margin-left: 0;
-}
-
+/* The first column should align with normal text. So we cannot use
+   border-spacing. */
 table.params {
     border-spacing: 0;
 }
 
+/* Add some padding instead of border-spacing */
 td.paramname {
     padding: 1px 1em 1px 0;
 }
 
-dl.literature dt {
-    clear: left;
-    float: left;
-    font-size: 100%;
-    font-weight: normal;
-    padding-right: 10px;
-    text-align: right;
-    width: 30px;
+/* We move the bar a out of the text frame, so that the text aligns well with
+   the rest. Note: margin-left + border-width-left + padding-left = 0 */
+dl.note, dl.warning, dl.attention, dl.pre, dl.post, dl.invariant, dl.deprecated, dl.todo, dl.test, dl.bug
+{
+    margin-left: -6px;
+    padding-left: 2px;
 }
 
 EOF
