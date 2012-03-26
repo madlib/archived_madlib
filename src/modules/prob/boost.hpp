f855e8b6b95db72c6f3dd7c4bb4fef2dc80e6c6f
Kevin Yun Liu <kevin.liu4@emc.com>
2012-03-26 20:22:56 +0800

    Add Normal Probability Density function

--- src/modules/prob/boost.hpp
+++ src/modules/prob/boost.hpp
@@ -18,8 +18,15 @@ DECLARE_UDF(chi_squared_cdf)
  */
 DECLARE_UDF(normal_cdf)
 
+/**
+ * @brief Normal probability density function
+ */
+DECLARE_UDF(normal_pdf)
 
+#if !defined(DECLARE_LIBRARY_EXPORTS)
 double chiSquaredCDF(int64_t nu, double t);
 double normalCDF(double t);
+double normalPDF(double t, double mu = 0.0, double sigma = 1.0);
+#endif // !defined(DECLARE_LIBRARY_EXPORTS)
 
 #endif // workaround for Doxygen
