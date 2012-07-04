3288c3dfad33ca67ed34e0d0ce3f5cac83e92da5
Kun Li <kli@cise.ufl.edu>
2012-07-04 17:16:31 -0400

    d

--- src/modules/crf/linear_crf_train.hpp
+++ src/modules/crf/linear_crf_train.hpp
@@ -16,4 +16,4 @@ DECLARE_UDF(crf, linear_crf_step_merge_states)
 /**
  * @brief Logistic crfion (conjugate-gradient step): Final function
  */
-DECLARE_UDF(crf, linear_crf__step_final)
+DECLARE_UDF(crf, linear_crf_step_final)
