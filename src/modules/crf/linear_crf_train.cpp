182fe80f56a36d1fc2cd2f4f9fec4b11f56c535d
Kun Li <kli@cise.ufl.edu>
2012-07-04 23:03:02 -0400

    d

--- src/modules/crf/linear_crf_train.cpp
+++ src/modules/crf/linear_crf_train.cpp
@@ -31,19 +31,6 @@ AnyType stateToResult(const Allocator &inAllocator,
                       const HandleMap<const ColumnVector, TransparentHandle<double> > &inlambda,
                       double loglikelihood);
 
-/**
- * @brief Inter- and intra-iteration state for conjugate-gradient method for
- *        logistic crfion
- *
- * TransitionState encapsualtes the transition state during the
- * logistic-crfion aggregate function. To the database, the state is
- * std::exposed as a single DOUBLE PRECISION array, to the C++ code it is a proper
- * object containing scalars and vectors.
- *
- * Note: We assume that the DOUBLE PRECISION array is initialized by the
- * database with length at least 5, and all elemenets are 0.
- *
- */
 template <class Handle>
 class GradientTransitionState {
     template <class OtherHandle>
@@ -56,30 +43,16 @@ public:
         rebind(static_cast<uint16_t>(mStorage[1]),static_cast<uint16_t>(mStorage[2]));
     }
 
-    /**
-     * @brief Convert to backend representation
-     *
-     * We define this function so that we can use State in the
-     * argument list and as a return type.
-     */
     inline operator AnyType() const {
         return mStorage;
     }
 
-    /**
-     * @brief Initialize the conjugate-gradient state.
-     *
-     * This function is only called for the first iteration, for the first row.
-     */
     inline void initialize(const Allocator &inAllocator, uint16_t num_features, uint16_t num_labels) {
         mStorage = inAllocator.allocateArray<double, dbal::AggregateContext,
         dbal::DoZero, dbal::ThrowBadAlloc>(arraySize(num_features,num_labels));
         rebind(num_features,num_labels);
     }
 
-    /**
-     * @brief We need to support assigning the previous state
-     */
     template <class OtherHandle>
     GradientTransitionState &operator=(
         const GradientTransitionState<OtherHandle> &inOtherState) {
@@ -89,27 +62,18 @@ public:
         return *this;
     }
 
-    /**
-     * @brief Merge with another State object by copying the intra-iteration
-     *     fields
-     */
     template <class OtherHandle>
     GradientTransitionState &operator+=(
         const GradientTransitionState<OtherHandle> &inOtherState) {
-
         if (mStorage.size() != inOtherState.mStorage.size())
             throw std::logic_error("Internal error: Incompatible transition "
                                    "states");
-
         numRows += inOtherState.numRows;
         grad_new += inOtherState.grad_new;
         loglikelihood += inOtherState.loglikelihood;
         return *this;
     }
 
-    /**
-     * @brief Reset the inter-iteration fields.
-     */
     inline void reset() {
         numRows = 0;
         grad_new.fill(0);
@@ -122,25 +86,6 @@ private:
         return 4 + 6 * num_features + num_labels * num_labels + 4 * num_labels + 1;
     }
 
-    /**
-     * @brief Rebind to a new storage array
-     *
-     * @param inWidthOfX The number of independent variables.
-     *
-     * Array layout (iteration refers to one aggregate-function call):
-     * Inter-iteration components (updated in final function):
-     * - 0: iteration (current iteration)
-     * - 1: widthOfX (number of coefficients)
-     * - 2: coef (vector of coefficients)
-     * - 2 + widthOfX: dir (direction)
-     * - 2 + 2 * widthOfX: grad (gradient)
-     * - 2 + 3 * widthOfX: beta (scale factor)
-     *
-     * Intra-iteration components (updated in transition step):
-     * - 3 + 3 * widthOfX: numRows (number of rows already processed in this iteration)
-     * - 4 + 3 * widthOfX: grad_new (intermediate value for gradient)
-     * - 4 + widthOfX * widthOfX + 4 * widthOfX: loglikelihood ( ln(l(c)) )
-     */
     void rebind(uint32_t num_features, uint32_t num_labels) {
         iteration.rebind(&mStorage[0]);
         num_features.rebind(&mStorage[1]);
@@ -163,8 +108,6 @@ private:
     Handle mStorage;
 
 public:
-    // this control the status information reported during training
-
     typename HandleTraits<Handle>::ReferenceToUInt32 iteration;
     typename HandleTraits<Handle>::ReferenceToUInt32 num_features;
     typename HandleTraits<Handle>::ReferenceToUInt16 num_labels;
@@ -183,10 +126,6 @@ public:
     typename HandleTraits<Handle>::ReferenceToUInt64 numRows;
 };
 
-
-/**
- * @brief Perform the logistic-crfion transition step
- */
 AnyType
 linear_crf_step_transition::run(AnyType &args) {
     GradientTransitionState<MutableArrayHandle<double> > state = args[0];
@@ -195,7 +134,6 @@ linear_crf_step_transition::run(AnyType &args) {
     HandleMap<const ColumnVector> prevLabel = args[3].getAs<ArrayHandle<double> >();
     HandleMap<const ColumnVector> currLabel = args[4].getAs<ArrayHandle<double> >();
     double seq_len_double = args[5].getAs<double>();
-    
     if (state.numRows == 0) {
         state.initialize(*this, state.num_features, state.num_labels);
         if (!args[3].isNull()) {
@@ -214,7 +152,7 @@ linear_crf_step_transition::run(AnyType &args) {
     scale(seq_len - 1) = true ? state.num_labels : 1;//TODO
     betas(seq_len - 1) = (1.0 / scale(seq_len - 1));
 
-    size_t index=0; 
+    size_t index=0;
     for (size_t i = seq_len - 1; i > 0; i--) {
         compute_log_Mi(features,prevLabel, currLabel, featureType,state.Mi,state.Vi,state.lambda,index,state.num_labels,true);
         *temp = *(betas[i]);
@@ -237,10 +175,10 @@ linear_crf_step_transition::run(AnyType &args) {
         }
 
         while (features(index)!=-1) {
-        size_t f_index = features(index);
-        size_t prev_index = prevLabel(index);
-        size_t curr_index = currLabel(index);
-        size_t f_type =  featureType(index);
+            size_t f_index = features(index);
+            size_t prev_index = prevLabel(index);
+            size_t curr_index = currLabel(index);
+            size_t f_type =  featureType(index);
             if (f_type == 0 || f_type == 1) {
                 state.gradlogli(f_index) += 1;
                 state.grad_new += state.lambda(f_index) * 1;
@@ -249,7 +187,7 @@ linear_crf_step_transition::run(AnyType &args) {
                 state.ExpF(f_index) += state.next_alpha(curr_index) * 1 * (betas[j])[curr_index(index)];
             } else if (f_type == 0) {
                 state.ExpF(f_index) += state.alpha[prev_index] * state.Vi(curr_index) * state.Mi(prev_index,curr_index)
-                                     * 1 * (*(betas[j]))(curr_index);
+                                       * 1 * (*(betas[j]))(curr_index);
             }
         }
         state.alpha = state.next_alpha;
@@ -309,63 +247,63 @@ linear_crf_step_final::run(AnyType &args) {
     for (size_t i = 0; i < state.num_features; i++) {
         state.gradlogli(i) = -1 * state.lambda(i) / 1;//TODO
         state.loglikelihood -= (state.lambda(i) * state.lambda(i)) / (2 * 1);
-     }
-     state.gradlogli+=state.grad_new;
-        //invole lbfgs algorithm
-        lbfgs(&state.num_features,&state.m_for_hessian,state.lambda,&state.loglikelihood,state.gradlogli,&state.diagco,
-              state.diag,state.eps_for_convergence,&state.xtol,state.ws,&state.iflog);
-        // checking after calling LBFGS
-        if (state.iflag < 0) {
-            // LBFGS error
-            printf("LBFGS routine encounters an error\n");
-        }
-        state.iteration++;
-        return state;
     }
-
-    /**
-     * @brief Return the difference in log-likelihood between two states
-     */
-    AnyType
-    internal_linear_crf_step_distance::run(AnyType &args) {
-        GradientTransitionState<ArrayHandle<double> > stateLeft = args[0];
-        GradientTransitionState<ArrayHandle<double> > stateRight = args[1];
-        return std::abs(stateLeft.loglikelihood - stateRight.loglikelihood);
+    state.gradlogli+=state.grad_new;
+    //invole lbfgs algorithm
+    lbfgs(&state.num_features,&state.m_for_hessian,state.lambda,&state.loglikelihood,state.gradlogli,&state.diagco,
+          state.diag,state.eps_for_convergence,&state.xtol,state.ws,&state.iflog);
+    // checking after calling LBFGS
+    if (state.iflag < 0) {
+        // LBFGS error
+        printf("LBFGS routine encounters an error\n");
     }
+    state.iteration++;
+    return state;
+}
 
-    /**
-     * @brief Return the coefficients and diagnostic statistics of the state
-     */
-    AnyType
-    internal_linear_crf_result::run(AnyType &args) {
-        GradientTransitionState<ArrayHandle<double> > state = args[0];
-        return stateToResult(*this, state.lambda, state.loglikelihood);
-    }
+/**
+ * @brief Return the difference in log-likelihood between two states
+ */
+AnyType
+internal_linear_crf_step_distance::run(AnyType &args) {
+    GradientTransitionState<ArrayHandle<double> > stateLeft = args[0];
+    GradientTransitionState<ArrayHandle<double> > stateRight = args[1];
+    return std::abs(stateLeft.loglikelihood - stateRight.loglikelihood);
+}
 
-    /**
-     * @brief Compute the diagnostic statistics
-     *
-     * This function wraps the common parts of computing the results for both the
-     * CG and the IRLS method.
-     */
-    AnyType stateToResult(
-        const Allocator &inAllocator,
-        const HandleMap<const ColumnVector, TransparentHandle<double> > &inlambda,
-        double loglikelihood) {
-        // FIXME: We currently need to copy the coefficient to a native array
-        // This should be transparent to user code
-        HandleMap<ColumnVector> lambda(
-            inAllocator.allocateArray<double>(inlambda.size()));
-        lambda = inlambda;
+/**
+ * @brief Return the coefficients and diagnostic statistics of the state
+ */
+AnyType
+internal_linear_crf_result::run(AnyType &args) {
+    GradientTransitionState<ArrayHandle<double> > state = args[0];
+    return stateToResult(*this, state.lambda, state.loglikelihood);
+}
 
-        // Return all coefficients, standard errors, etc. in a tuple
-        AnyType tuple;
-        tuple << loglikelihood << lambda;
-        return tuple;
-    }
+/**
+ * @brief Compute the diagnostic statistics
+ *
+ * This function wraps the common parts of computing the results for both the
+ * CG and the IRLS method.
+ */
+AnyType stateToResult(
+    const Allocator &inAllocator,
+    const HandleMap<const ColumnVector, TransparentHandle<double> > &inlambda,
+    double loglikelihood) {
+    // FIXME: We currently need to copy the coefficient to a native array
+    // This should be transparent to user code
+    HandleMap<ColumnVector> lambda(
+        inAllocator.allocateArray<double>(inlambda.size()));
+    lambda = inlambda;
 
-    // compute log Mi (first-order Markov)
-    AnyType compute_log_Mi(
+    // Return all coefficients, standard errors, etc. in a tuple
+    AnyType tuple;
+    tuple << loglikelihood << lambda;
+    return tuple;
+}
+
+// compute log Mi (first-order Markov)
+AnyType compute_log_Mi(
     const HandleMap<const ColumnVector, TransparentHandle<double> > &features,
     const HandleMap<const ColumnVector, TransparentHandle<double> > &prevLabel,
     const HandleMap<const ColumnVector, TransparentHandle<double> > &currLabel,
@@ -382,25 +320,25 @@ linear_crf_step_final::run(AnyType &args) {
         size_t prev_index = prevLabel(index);
         size_t curr_index = currLabel(index);
         size_t f_type =  featureType(index);
-	if (f_type == 0) {
-	    // state feature
-	    Vi(curr_index) += lambda(f_index);
-	} else if (f_type == 1) /* if (pos > 0)*/ {
-	    Mi(prev_index, curr_index) += lambda(f_index);
-	}
+        if (f_type == 0) {
+            // state feature
+            Vi(curr_index) += lambda(f_index);
+        } else if (f_type == 1) { /* if (pos > 0)*/
+            Mi(prev_index, curr_index) += lambda(f_index);
+        }
         index++;
     }
-    
+
     // take exponential operator
     if (is_exp) {
-	for (size_t i = 0; i < num_labels; i++) {
-	    // update for Vi
-	    Vi(i) = std::exp(Vi(i));
-	    // update for Mi
-	    for (size_t j = 0; j < num_labels; j++) {
-		Mi(i, j) = std::exp(Mi(i, j));
-	    }
-	}
+        for (size_t i = 0; i < num_labels; i++) {
+            // update for Vi
+            Vi(i) = std::exp(Vi(i));
+            // update for Mi
+            for (size_t j = 0; j < num_labels; j++) {
+                Mi(i, j) = std::exp(Mi(i, j));
+            }
+        }
     }
 }
 
