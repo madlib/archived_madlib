c72ea3cc46fd2e7edcdbc932a1b4ebed54332a39
Kun Li <kli@cise.ufl.edu>
2012-07-19 15:03:10 -0400

    d

--- src/modules/crf/lbfgs.h
+++ src/modules/crf/lbfgs.h
@@ -66,65 +66,12 @@
 */
 class drwnOptimizer {
  public:
-    unsigned _n;  //!< dimension of optimization problem (i.e., \f$\mathbb{R}^n\f$)
-    double *_x;   //!< current feasible solution in \f$\mathbb{R}^n\f$
-    double *_df;  //!< gradient at _x  in \f$\mathbb{R}^n\f$
-
- public:
-    //! default constructor
-    drwnOptimizer();
-    //! construct a problem with dimension \b n
-    drwnOptimizer(unsigned n);
-    //! copy constructor
-    drwnOptimizer(const drwnOptimizer& o);
-    virtual ~drwnOptimizer();
-
-    //! initialize an optimization problem of size \b n possibly with feasible
-    //! starting point \b x \f$\mathbb{R}^n\f$ (or zero)
-    void initialize(unsigned n, const double *x = NULL);
-    //! initialize an optimization problem at feasible starting point \b x in
-    //! \f$\mathbb{R}^n\f$ (or zero)
-    void initialize(const double *x = NULL);
-
-    //! solve the optimization problem for up to \b maxiter iterations to precision \b tol
-    //! calls \ref monitor function after each iteration if \b bMonitor is \a true
-    double solve(unsigned maxiter, double tol = 1.0e-3, bool bMonitor = false);
-
-    //! returns value of objective function at point \b x
-    virtual double objective(const double *x) const = 0;
-    //! populates gradient of objective function at point \b x
-    virtual void gradient(const double *x, double *df) const = 0;
-    //! returns value of objective function and populates gradient \b df at point \b x
-    virtual double objectiveAndGradient(const double *x, double *df) const {
-	gradient(x, df); return objective(x);
-    }
-
-    //! dimension of optimization problem
-    inline unsigned size() const { return _n; }
-    //! returns the \a i-th component of the current solution
-    inline double operator[](unsigned i) const { return _x[i]; }
-    //! returns a reference to the \a i-th component of the current solution
-    inline double& operator[](unsigned i) { return _x[i]; }
-
-    //! callback for each iteration during optimization (if \b bMonitor is \a true)
-    virtual void monitor(unsigned iter, double objValue);
-
- protected:
-    ///@cond
-    enum  drwnLBFGSResult {
-        DRWN_LBFGS_ERROR, DRWN_LBFGS_MAX_ITERS, DRWN_LBFGS_CONVERGED_F,
-        DRWN_LBFGS_CONVERGED_G, DRWN_LBFGS_CONVERGED_X
-    };
-
-    drwnLBFGSResult lbfgsMinimize(int m, unsigned maxiter,
         double epsg, double epsf, double epsx, bool bMonitor);
-    bool lbfgsSearch(double &f, const Eigen::VectorXd &s,
+    static bool lbfgsSearch(double &f, const Eigen::VectorXd &s,
         double& stp, Eigen::VectorXd& diag);
-    bool lbfgsStep(double& stx, double& fx, double& dx,
+    static bool lbfgsStep(double& stx, double& fx, double& dx,
         double& sty, double& fy, double& dy,
         double& stp, const double& fp, const double& dp,
         bool& brackt, const double& stmin, const double& stmax);
     ///@endcond
 };
-
-
