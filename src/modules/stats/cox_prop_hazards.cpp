e1568edf5c8a485364f5c934ee5d70c46daa8b9f
Srikrishna Sridhar <srikrishna097@gmail.com>
2012-08-05 21:59:24 -0500

    Tried to add the intermediate table for Cox prop hazards.

--- src/modules/stats/cox_prop_hazards.cpp
+++ src/modules/stats/cox_prop_hazards.cpp
@@ -30,13 +30,23 @@ AnyType stateToResult(
 	const HandleMap<const ColumnVector, TransparentHandle<double> >& inCoef,
 	double logLikelihood);
 
+// Internal functions
+AnyType intermediate_stateToResult(
+		const double inTimeDeath,
+		const HandleMap<const ColumnVector, TransparentHandle<double> >& inCoef,
+		const double inExp_coef_x,
+		const HandleMap<const ColumnVector, TransparentHandle<double> >& inX_exp_coef_x,
+		const HandleMap<const Matrix, TransparentHandle<double> >& inX_xTrans_exp_coef_x
+		);
+
+
 
 
 /**
  * @brief Transition state for the Cox Proportional Hazards
  *
  * TransitionState encapsulates the transition state during the
- * linear-regression aggregate functions. To the database, the state is exposed
+ * aggregate functions. To the database, the state is exposed
  * as a single DOUBLE PRECISION array, to the C++ code it is a proper object
  * containing scalars, a vector, and a matrix.
  *
@@ -193,7 +203,7 @@ public:
 
 
 /**
- * @brief Gradient descent transition step for Cox Proportional Hazards
+ * @brief Newton method transition step for Cox Proportional Hazards
  *
  * @param args
  *
@@ -247,7 +257,7 @@ AnyType cox_prop_hazards_step_transition::run(AnyType &args) {
 
 
 /**
- * @brief Gradient descent final step for Cox Proportional Hazards
+ * @brief Newton method final step for Cox Proportional Hazards
  *
  */
 AnyType cox_prop_hazards_step_final::run(AnyType &args) {
@@ -304,6 +314,261 @@ AnyType stateToResult(
     // Return all coefficients, standard errors, etc. in a tuple
     AnyType tuple;
 		tuple << inCoef << logLikelihood;
+		
+    return tuple;
+}
+
+
+
+
+
+
+
+
+
+/**
+ * @brief Transition state of Intermadiate Cox Proportional Hazard computations
+ *
+ * TransitionState encapsulates the transition state during the
+ * intermediate computations. To the database, the state is exposed
+ * as a single DOUBLE PRECISION array, to the C++ code it is a proper object
+ * containing scalars, a vector, and a matrix.
+ *
+ * Note: We assume that the DOUBLE PRECISION array is initialized by the
+ * database with length at least 5, and all elements are 0.
+ */
+template <class Handle>
+class IntermediateCoxPropHazardsTransitionState {
+
+    template <class OtherHandle>
+    friend class IntermediateCoxPropHazardsTransitionState;
+
+public:
+    IntermediateCoxPropHazardsTransitionState(const AnyType &inArray)
+      : mStorage(inArray.getAs<Handle>()) {
+
+        rebind(static_cast<uint16_t>(mStorage[1]));
+    }
+
+    /**
+     * @brief Convert to backend representation
+     *
+     * We define this function so that we can use TransitionState in the argument
+     * list and as a return type.   */
+    inline operator AnyType() const {
+        return mStorage;
+    }
+
+    /**
+     * @brief Initialize the transition state. Only called for first row.
+     *
+     * @param inAllocator Allocator for the memory transition state. Must fill
+     *     the memory block with zeros.
+     * @param inWidthOfX Number of independent variables. The first row of data
+     *     determines the size of the transition state. This size is a quadratic
+     *     function of inWidthOfX.
+     */
+    inline void initialize(const Allocator &inAllocator, uint16_t inWidthOfX) {
+        mStorage = inAllocator.allocateArray<double, dbal::AggregateContext,
+            dbal::DoZero, dbal::ThrowBadAlloc>(arraySize(inWidthOfX));
+        rebind(inWidthOfX);
+        widthOfX = inWidthOfX;
+    }
+
+    /**
+     * @brief We need to support assigning the previous state
+     */
+    template <class OtherHandle>
+    IntermediateCoxPropHazardsTransitionState &operator=(
+			const IntermediateCoxPropHazardsTransitionState<OtherHandle> &inOtherState) {
+
+			for (size_t i = 0; i < mStorage.size(); i++)
+					mStorage[i] = inOtherState.mStorage[i];
+			return *this;
+    }
+
+
+    /**
+     * @brief Merge with another State object by copying the intra-iteration
+     *     fields
+     */
+    template <class OtherHandle>
+    IntermediateCoxPropHazardsTransitionState &operator+=(
+        const CoxPropHazardsTransitionState<OtherHandle> &inOtherState) {
+
+        if (mStorage.size() != inOtherState.mStorage.size() ||
+            widthOfX != inOtherState.widthOfX)
+            throw std::logic_error("Internal error: Incompatible transition "
+                "states");
+
+        numRows += inOtherState.numRows;
+
+        return *this;
+    }
+
+    /**
+     * @brief Reset the inter-iteration fields.
+     */
+    inline void reset() {
+        numRows = 0;
+				exp_coef_x = 0;
+				x_exp_coef_x.fill(0);
+				x_xTrans_exp_coef_x.fill(0);
+    }
+
+
+private:
+    static inline size_t arraySize(const uint16_t inWidthOfX) {
+        return 4 + 2*inWidthOfX + inWidthOfX*inWidthOfX;
+    }
+
+    /**
+     * @brief Rebind to a new storage array
+     *
+     * @param inWidthOfX The number of independent variables.
+     *
+     * Array layout:
+		 * Inter iteration components (updated in the final step)
+     * - 0: numRows (number of rows seen so far)
+     * - 1: widthOfX (number of features)
+     * - 2: coef (multipliers for each of the features)
+     * - 4: timeDeath (time of death)
+		 
+		 * Intra interation components (updated in the current interation)
+		 * - 2 + widthofX: s (precomputation for gradient & hessian)
+     * - 2 + 2*widthofX: h (precomputation for gradient)
+     * - 2 + 3*widthofX: v (precomputation for hessian)
+		 *
+     */
+    void rebind(uint16_t inWidthOfX) {
+        numRows.rebind(&mStorage[0]);
+        widthOfX.rebind(&mStorage[1]);
+        timeDeath.rebind(&mStorage[2]);
+        coef.rebind(&mStorage[3],inWidthOfX);
+
+        exp_coef_x.rebind(&mStorage[3+inWidthOfX]);
+        x_exp_coef_x.rebind(&mStorage[4+inWidthOfX], inWidthOfX);
+        x_xTrans_exp_coef_x.rebind(&mStorage[4+2*inWidthOfX],
+																					inWidthOfX, inWidthOfX);
+
+    }
+
+    Handle mStorage;
+
+public:
+    typename HandleTraits<Handle>::ReferenceToUInt64 numRows;
+    typename HandleTraits<Handle>::ReferenceToUInt16 widthOfX;
+    typename HandleTraits<Handle>::ReferenceToDouble timeDeath;
+		typename HandleTraits<Handle>::ColumnVectorTransparentHandleMap coef;
+
+    typename HandleTraits<Handle>::ReferenceToDouble exp_coef_x;
+		typename HandleTraits<Handle>::ColumnVectorTransparentHandleMap x_exp_coef_x;
+		typename HandleTraits<Handle>::MatrixTransparentHandleMap x_xTrans_exp_coef_x;
+		
+};
+
+
+/**
+ * @brief Newton method transition step for Cox Proportional Hazards
+ *
+ * @param args
+ *
+ * Arguments (Matched with PSQL wrapped)
+ * - 0: Current State
+ * - 1: X value (Column Vector)
+ * - 2: coef value (Column Vector)
+ * - 3: timeDeath value (Column Vector)
+ * - 4: Previous State
+*/
+
+AnyType intermediate_cox_prop_hazards_step_transition::run(AnyType &args) {
+
+    IntermediateCoxPropHazardsTransitionState<MutableArrayHandle<double> > state=args[0];
+    MappedColumnVector x = args[1].getAs<MappedColumnVector>();
+    double timeDeath = args[2].getAs<double>();
+    MappedColumnVector coef = args[3].getAs<MappedColumnVector>();
+
+    // The following check was added with MADLIB-138.
+    if (!isfinite(x)){
+        throw std::domain_error("Design matrix is not finite.");
+		}
+
+    // Now do the transition step.
+    if (state.numRows == 0) {
+        if (x.size() > std::numeric_limits<uint16_t>::max())
+            throw std::domain_error("Number of independent variables cannot be "
+                "larger than 65535.");
+
+        state.initialize(*this, static_cast<uint16_t>(x.size()));
+        if (!args[4].isNull()) {
+            IntermediateCoxPropHazardsTransitionState<ArrayHandle<double> > previousState = args[4];
+            state = previousState;
+            state.reset();
+        }
+
+    }
+
+    state.numRows++;
+		double s = std::exp(trans(coef)*x);
+		
+		state.timeDeath += timeDeath;
+		state.exp_coef_x += s;
+		state.coef += coef;
+		state.x_exp_coef_x += s*x;
+		state.x_xTrans_exp_coef_x += x * trans(x) * s;
+		
+    return state;
+}
+
+
+/**
+ * @brief Newton method final step for Cox Proportional Hazards
+ *
+ */
+AnyType intermediate_cox_prop_hazards_step_final::run(AnyType &args) {
+
+    IntermediateCoxPropHazardsTransitionState<MutableArrayHandle<double> > state = args[0];
+
+    // If we haven't seen any data, just return Null. 
+    if (state.numRows == 0)
+        return Null();
+
+
+    // Return all coefficients etc. in a tuple
+    return state;
+}
+
+/**
+ * @brief Return the coefficients and diagnostic statistics of the state
+ */
+AnyType intermediate_internal_cox_prop_hazards_result::run(AnyType &args) {
+
+    IntermediateCoxPropHazardsTransitionState<ArrayHandle<double> > state = args[0];
+    double timeDeath = args[1].getAs<double>();
+		
+    return intermediate_stateToResult(
+				timeDeath,
+				state.coef,
+				state.exp_coef_x,
+				state.x_exp_coef_x,
+				state.x_xTrans_exp_coef_x);
+}
+
+/**
+ * @brief Compute the diagnostic statistics
+ *
+ */
+AnyType intermediate_stateToResult(
+		double inTimeDeath,
+		const HandleMap<const ColumnVector, TransparentHandle<double> > &inCoef,
+		double &inExp_coef_x,
+		const HandleMap<const ColumnVector, TransparentHandle<double> > &inX_exp_coef_x,
+		const HandleMap<const Matrix, TransparentHandle<double> >& inX_xTrans_exp_coef_x){
+
+    // Return all coefficients, standard errors, etc. in a tuple
+    AnyType tuple;
+		tuple << inTimeDeath << inCoef << inExp_coef_x
+					<< inX_exp_coef_x << inX_xTrans_exp_coef_x;
 		
     return tuple;
 }
--- src/modules/stats/cox_prop_hazards.hpp
+++ src/modules/stats/cox_prop_hazards.hpp
@@ -23,3 +23,19 @@ DECLARE_UDF(stats, internal_cox_prop_hazards_result)
  * @brief Cox proportional Hazards: Step Distance
  */
 DECLARE_UDF(stats, internal_cox_prop_hazards_step_distance)
+
+/**
+ * @brief Intermadiate Cox Proportional Hazard computation: Transition function
+ */
+DECLARE_UDF(stats, intermediate_cox_prop_hazards_step_transition)
+
+/**
+ * @brief Intermadiate Cox Proportional Hazard computation: Final function
+ */
+DECLARE_UDF(stats, intermediate_cox_prop_hazards_step_final)
+
+/**
+ * @brief Intermadiate Cox Proportional Hazard computation: Intermadiate Results
+ */
+DECLARE_UDF(stats, intermediate_internal_cox_prop_hazards_result)
+
