b51bdaf1d3c24118001820eca3943dbe8a99d8c7
Srikrishna Sridhar <ssridhar@gopivotal.com>
2013-07-01 12:08:50 +0800

    Regress: Add Marginal Effect Estimates for multinomial logistic regression (w/o grouping)
    Pivotal ID: 51919995

--- src/modules/regress/multilogistic.hpp
+++ src/modules/regress/multilogistic.hpp
@@ -51,4 +51,19 @@ DECLARE_UDF(regress, mlogregr_robust_step_merge_states)
  */
 DECLARE_UDF(regress, mlogregr_robust_step_final)
 
+/**
+ * @brief Marginal Effects Logistic regression step: Transition function
+ */
+DECLARE_UDF(regress, mlogregr_marginal_step_transition)
+
+/**
+ * @brief Marginal effects Logistic regression step: State merge function
+ */
+DECLARE_UDF(regress, mlogregr_marginal_step_merge_states)
+
+/**
+ * @brief Marginal Effects Logistic regression step: Final function
+ */
+DECLARE_UDF(regress, mlogregr_marginal_step_final)
+
 
