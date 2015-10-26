647c6f2829f98206ebad4b537bb4deb6623a70ce
Max Yang <myang@pivotal.io>
2015-10-26 13:56:15 -0700

    New module: Add stemmer function
    Pivotal Tracker: #98065650
    
    Adds a new function that computes the root word of any text input.

---
+++ methods/stemmer/src/pg_gp/porter_stemmer.c
@@ -0,0 +1,112 @@
+/*
+ * The library provides a simple C API.  Essentially, a new stemmer can
+ * be obtained by using "sb_stemmer_new".  "sb_stemmer_stem" is then
+ * used to stem a word, "sb_stemmer_length" returns the stemmed
+ * length of the last word processed, and "sb_stemmer_delete" is
+ * used to delete a stemmer.
+ *
+ * Creating a stemmer is a relatively expensive operation - the expected
+ * usage pattern is that a new stemmer is created when needed, used
+ * to stem many words, and deleted after some time.
+ *
+ * Stemmers are re-entrant, but not threadsafe.  In other words, if
+ * you wish to access the same stemmer object from multiple threads,
+ * you must ensure that all access is protected by a mutex or similar
+ * device.
+ *
+ * libstemmer does not currently incorporate any mechanism for caching the results
+ * of stemming operations.  Such caching can greatly increase the performance of a
+ * stemmer under certain situations, so suitable patches will be considered for
+ * inclusion.
+ *
+ * The standard libstemmer sources contain an algorithm for each of the supported
+ * languages.  The algorithm may be selected using the english name of the
+ * language, or using the 2 or 3 letter ISO 639 language codes.  In addition,
+ * the traditional "Porter" stemming algorithm for english is included for
+ * backwards compatibility purposes, but we recommend use of the "English"
+ * stemmer in preference for new projects.
+ */
+
+/*
+ * Postgres headers
+ */
+#include <postgres.h>
+#include <catalog/pg_type.h>
+#include <fmgr.h>
+#include <sys/time.h>
+#include <funcapi.h>
+#include <utils/builtins.h>
+#include <utils/array.h>
+
+/*
+ * libstemmer header
+ */
+#include "libstemmer/include/libstemmer.h"
+
+static const char * stem_token_text(struct sb_stemmer *stemmer, text *token) {
+    char * stemmed_token = TextDatumGetCString(PointerGetDatum(token));
+    if (strcmp(stemmed_token, "") == 0) {
+        return "";
+    }
+    const sb_symbol *stemmed = sb_stemmer_stem(stemmer,
+                                               (const sb_symbol *) stemmed_token,
+                                               strlen(stemmed_token));
+    return (const char*) stemmed;
+}
+
+/*
+ * Plug in stemmer call to be invoked via SQL
+ */
+PG_FUNCTION_INFO_V1(stem_token);
+Datum stem_token(PG_FUNCTION_ARGS)
+{
+    if (PG_ARGISNULL(0)){
+        PG_RETURN_NULL();
+    }
+    text * org_token = PG_GETARG_TEXT_P(0);
+    struct sb_stemmer *stemmer = sb_stemmer_new(
+            "english" /* language */, NULL /* language encoding NULL for UTF-8 */);
+    Assert(stemmer);
+    text *stemmed = cstring_to_text(stem_token_text(stemmer, org_token));
+    sb_stemmer_delete(stemmer);
+    PG_RETURN_TEXT_P(stemmed);
+}
+
+/*
+ *  Stemmer function processing text[] input
+ */
+PG_FUNCTION_INFO_V1(stem_token_arr);
+Datum stem_token_arr(PG_FUNCTION_ARGS)
+{
+    if (PG_ARGISNULL(0)){
+        PG_RETURN_NULL();
+    }
+    /* Prepare elements to receive input text[] */
+    ArrayType *arr = PG_GETARG_ARRAYTYPE_P(0);
+    Datum *dtum;
+    bool *nulls;
+    int ndim;
+    /* Deconstruct input text[] */
+    deconstruct_array(arr, TEXTOID, -1, false, 'i', &dtum, &nulls, &ndim);
+    /* Prepare stemmer */
+    struct sb_stemmer *stemmer = sb_stemmer_new(
+            "english" /* language */, NULL /* language encoding NULL for UTF-8 */);
+    Assert(stemmer);
+
+    /* Call stemming code */
+    text **result = (text **) palloc(ndim * sizeof(text * ));
+    for(int i=0; i< ndim; i++) {
+        text *token = dtum[i] == 0 ? NULL : DatumGetTextP(dtum[i]);
+        char *empty;
+        if(token == NULL) {
+            empty =  (char *)palloc(sizeof(char));
+            empty[0] = '\0';
+        }
+        result[i] = (token == NULL ?
+                     cstring_to_text(empty) :
+                     cstring_to_text(stem_token_text(stemmer, token)));
+    }
+    ArrayType *res = construct_array((Datum*)result, ndim, TEXTOID, -1, false, 'i');
+    sb_stemmer_delete(stemmer);
+    PG_RETURN_ARRAYTYPE_P(res);
+}
