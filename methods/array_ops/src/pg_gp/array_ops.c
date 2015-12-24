33c0a72c69e9c986067eddad99c8977c7874e9f9
Rahul Iyer <riyer@pivotal.io>
2015-12-24 14:15:36 -0800

    Path: Allow multiple matches in single partition
    JIRA: MADLIB-917
    
    Path query was rewritten to use array functions. This was necessary
    since multiple matches produced a set of matches for each group. This
    set value has to be unnest - this was not possible in GPDB since the
    GROUP BY placed the executor in a context that does not allow SRF.
    Solution was to avoid the unnest by using array functions for all
    computations and finally unnesting without the GROUP BY.
    
    Closes #21

--- methods/array_ops/src/pg_gp/array_ops.c
+++ methods/array_ops/src/pg_gp/array_ops.c
@@ -47,6 +47,8 @@ static ArrayType *General_2Array_to_Array(ArrayType *v1, ArrayType *v2,
         Datum(*element_function)(Datum,Oid,Datum,Oid,Datum,Oid));
 static ArrayType *General_Array_to_Array(ArrayType *v1, Datum value,
         Datum(*element_function)(Datum,Oid,Datum,Oid,Datum,Oid));
+static ArrayType *General_Array_to_Cumulative_Array(ArrayType *v1, Datum value,
+        Datum(*element_function)(Datum,Oid,Datum,Oid,Datum,Oid));
 static Datum General_2Array_to_Element(ArrayType *v1, ArrayType *v2,
         Datum(*element_function)(Datum,Oid,Datum,Oid,Datum,Oid),
         Datum(*finalize_function)(Datum,int,Oid));
@@ -491,6 +493,7 @@ array_sum(PG_FUNCTION_ARGS){
     return float8_datum_cast(DatumGetFloat8(res), element_type);
 }
 
+
 /*
  * This function returns sum of the array elements' absolute value.
  */
@@ -889,6 +892,36 @@ array_scalar_add(PG_FUNCTION_ARGS){
 
     PG_FREE_IF_COPY(v1, 0);
 
+    PG_RETURN_ARRAYTYPE_P(res);
+}
+
+/*
+ * This function returns the cumulative sum of the array elements.
+ */
+PG_FUNCTION_INFO_V1(array_cum_sum);
+Datum
+array_cum_sum(PG_FUNCTION_ARGS){
+    if (PG_ARGISNULL(0)) { PG_RETURN_NULL(); }
+
+    ArrayType *v = PG_GETARG_ARRAYTYPE_P(0);
+    ArrayType *res = General_Array_to_Cumulative_Array(v, Float8GetDatum(0.0), element_add);
+
+    PG_FREE_IF_COPY(v, 0);
+    PG_RETURN_ARRAYTYPE_P(res);
+}
+
+/*
+ * This function returns the cumulative product of the array elements.
+ */
+PG_FUNCTION_INFO_V1(array_cum_prod);
+Datum
+array_cum_prod(PG_FUNCTION_ARGS){
+    if (PG_ARGISNULL(0)) { PG_RETURN_NULL(); }
+
+    ArrayType *v = PG_GETARG_ARRAYTYPE_P(0);
+    ArrayType *res = General_Array_to_Cumulative_Array(v, Float8GetDatum(1.0), element_mult);
+
+    PG_FREE_IF_COPY(v, 0);
     PG_RETURN_ARRAYTYPE_P(res);
 }
 
@@ -1951,6 +1984,109 @@ General_Array_to_Array(
                                     elt2,
                                     element_type);
         resultp ++;
+    }
+
+    // construct return result
+    ArrayType *pgarray = construct_md_array(result,
+                                            NULL,
+                                            ndims,
+                                            dims,
+                                            lbs,
+                                            element_type,
+                                            type_size,
+                                            typbyval,
+                                            typalign);
+
+    pfree(result);
+    pfree(dims);
+    pfree(lbs);
+
+    return pgarray;
+}
+
+
+/*
+ * @brief Transforms an array to another array using cumulative operations.
+ *
+ * @param v1 Array.
+ * @param initial Parameter.
+ * @param element_function Map function.
+ * @returns Transformed array.
+ */
+ArrayType*
+General_Array_to_Cumulative_Array(
+        ArrayType *v1,
+        Datum initial,
+        Datum(*element_function)(Datum,Oid,Datum,Oid,Datum,Oid)) {
+
+    // dimensions
+    int ndims1 = ARR_NDIM(v1);
+    if (ndims1 == 0) {
+        elog(WARNING, "input are empty arrays.");
+        return v1;
+    }
+    int ndims = ndims1;
+    int *lbs1 = ARR_LBOUND(v1);
+    int *dims1 = ARR_DIMS(v1);
+    int *dims = (int *) palloc(ndims * sizeof(int));
+    int *lbs = (int *) palloc(ndims * sizeof(int));
+    int i = 0;
+    for (i = 0; i < ndims; i ++) {
+        dims[i] = dims1[i];
+        lbs[i] = lbs1[i];
+    }
+    int nitems = ArrayGetNItems(ndims, dims);
+
+    // nulls
+    if (ARR_HASNULL(v1)) {
+        ereport(ERROR, (errcode(ERRCODE_NULL_VALUE_NOT_ALLOWED),
+                        errmsg("arrays cannot contain nulls"),
+                        errdetail("Arrays with element value NULL are not allowed.")));
+    }
+
+    // type
+    Oid element_type = ARR_ELEMTYPE(v1);
+    TypeCacheEntry *typentry = lookup_type_cache(element_type,TYPECACHE_CMP_PROC_FINFO);
+    int type_size = typentry->typlen;
+    bool typbyval = typentry->typbyval;
+    char typalign = typentry->typalign;
+
+    // allocate
+    Datum *result = NULL;
+    switch (element_type) {
+        case INT2OID:
+        case INT4OID:
+        case INT8OID:
+        case FLOAT4OID:
+        case FLOAT8OID:
+        case NUMERICOID:
+            result = (Datum *)palloc(nitems * sizeof(Datum));break;
+        default:
+            ereport(ERROR, (errcode(ERRCODE_INVALID_PARAMETER_VALUE),
+                            errmsg("type is not supported"),
+                            errdetail("Arrays with element type %s are not supported.",
+                                      format_type_be(element_type))));
+            break;
+    }
+
+    // iterate
+    Datum *resultp = result;
+    char *dat1 = ARR_DATA_PTR(v1);
+    float prev_dat = DatumGetFloat8(initial);
+    for (i = 0; i < nitems; i ++) {
+        // iterate elt1
+        Datum elt1 = fetch_att(dat1, typbyval, type_size);
+        dat1 = att_addlength_pointer(dat1, type_size, dat1);
+        dat1 = (char *) att_align_nominal(dat1, typalign);
+
+        *resultp = element_function(elt1,
+                                    element_type,
+                                    elt1,         /* placeholder */
+                                    element_type, /* placeholder */
+                                    prev_dat,
+                                    element_type);
+        prev_dat = *resultp;
+        resultp++;
     }
 
     // construct return result
