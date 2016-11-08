82e56a46f6e902deca29580f6416bb1d60c9262a
Nandish Jayaram <njayaram@apache.org>
2016-11-08 15:49:01 -0800

    Build: Fix version used in rpm installation
    During MADlib installation a directory is created in `Versions` that
    contains the current version. A soft link called `Current` is created
    in the root for madpack to link to this directory.
    
    Versions that have a hyphen were invalid for gppkg and hence were
    updated to replace hyphen with an underscore. This led to discrepancy
    with the softlink. This commit fixes that bug by ensuring the softlink
    links to the right place.

--- deploy/rpm_post.sh
+++ deploy/rpm_post.sh
@@ -4,6 +4,12 @@ find $RPM_INSTALL_PREFIX/madlib/bin -depth -type d -exec rm -r {} \; 2>/dev/null
 find $RPM_INSTALL_PREFIX/madlib/doc -type d -exec cp -RPf {} $RPM_INSTALL_PREFIX/madlib/old_doc \; 2>/dev/null
 find $RPM_INSTALL_PREFIX/madlib/doc -depth -type d -exec rm -r {} \; 2>/dev/null
 
-ln -nsf $RPM_INSTALL_PREFIX/madlib/Versions/%{_madlib_version} $RPM_INSTALL_PREFIX/madlib/Current
+
+# RPM version is setup with underscore replaced for hyphen but
+# the actual version contains a hyphen. The symlink created on disk
+# is with a hyphen instead of the underscore.
+MADLIB_VERSION_NO_HYPHEN=%{_madlib_version}
+MADLIB_VERSION="${MADLIB_VERSION_NO_HYPHEN/_/-}"
+ln -nsf $RPM_INSTALL_PREFIX/madlib/Versions/$MADLIB_VERSION $RPM_INSTALL_PREFIX/madlib/Current
 ln -nsf $RPM_INSTALL_PREFIX/madlib/Current/bin $RPM_INSTALL_PREFIX/madlib/bin
 ln -nsf $RPM_INSTALL_PREFIX/madlib/Current/doc $RPM_INSTALL_PREFIX/madlib/doc
